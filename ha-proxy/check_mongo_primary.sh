b7e4c68682971e98c49851f5da7713660a02269d
dragonfall1370 <g.dragonfall1370@gmail.com>
2025-10-26 21:01:41 +0100

    fix mongosh in ha via docker build

--- ha-proxy/check_mongo_primary.sh
+++ ha-proxy/check_mongo_primary.sh
@@ -1,15 +1,12 @@
 #!/bin/sh
-# check_mongo_primary.sh <host> <port>
 HOST="$1"
 PORT="$2"
-
 USER="${MONGO_USER:-admin}"
 PASS="${MONGO_PASS:-}"
 AUTHDB="${MONGO_AUTH_DB:-admin}"
 
-# Return 0 (OK) only if node is primary
-mongosh --quiet \
+/usr/bin/mongosh --quiet \
   --host "$HOST" --port "$PORT" \
   -u "$USER" -p "$PASS" --authenticationDatabase "$AUTHDB" \
   --eval 'const r = db.hello(); print(r.isWritablePrimary === true ? "true" : "false")' \
-| grep -q true
+| /bin/grep -q true
